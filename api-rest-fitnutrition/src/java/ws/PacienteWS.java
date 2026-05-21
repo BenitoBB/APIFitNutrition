414c256ee095ca1eaea64f66eac3fcc56920b180
ChJulian13 <christianjulianjimenez13@outlook.com>
2026-05-21 16:40:21 -0600

    T306 - feat(paciente): agregar endpoint PUT de actualización en PacienteWS

--- api-rest-fitnutrition/src/java/ws/PacienteWS.java
+++ api-rest-fitnutrition/src/java/ws/PacienteWS.java
@@ -9,6 +9,7 @@ import dominio.PacienteImp;
 import dto.Respuesta;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -25,5 +26,15 @@ public class PacienteWS {
         Gson gson = new Gson();
         Paciente paciente = gson.fromJson(json, Paciente.class);
         return PacienteImp.registrarPaciente(paciente);
+    }
+    
+    @Path("actualizar")
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Respuesta actualizarPaciente(String json) {
+        Gson gson = new Gson();
+        Paciente paciente = gson.fromJson(json, Paciente.class);
+        return PacienteImp.actualizarPaciente(paciente);
     }
 }
