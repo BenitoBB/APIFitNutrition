85633b7ec59eebe8299a47b86dabf63aac79ade0
ChJulian13 <christianjulianjimenez13@outlook.com>
2026-05-21 16:28:48 -0600

    T304 - feat(paciente): agregar endpoint POST de registro en PacienteWS

---
+++ api-rest-fitnutrition/src/java/ws/PacienteWS.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ws;
+
+import com.google.gson.Gson;
+import dominio.PacienteImp;
+import dto.Respuesta;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import pojo.Paciente;
+
+@Path("paciente")
+public class PacienteWS {
+
+    @Path("registrar")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Respuesta registrarPaciente(String json) {
+        Gson gson = new Gson();
+        Paciente paciente = gson.fromJson(json, Paciente.class);
+        return PacienteImp.registrarPaciente(paciente);
+    }
+}
