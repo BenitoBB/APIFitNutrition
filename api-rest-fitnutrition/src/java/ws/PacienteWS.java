e6b50cbb307cdb0aa9eb9dc6bedcd907fb24527b
ChJulian13 <christianjulianjimenez13@outlook.com>
2026-05-21 18:16:59 -0600

    T307 - feat(paciente): agregar endpoint DELETE de baja en PacienteWS

--- api-rest-fitnutrition/src/java/ws/PacienteWS.java
+++ api-rest-fitnutrition/src/java/ws/PacienteWS.java
@@ -9,9 +9,11 @@ import dominio.PacienteImp;
 import dto.PacienteUpdateRequest;
 import dto.Respuesta;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import pojo.Paciente;
@@ -37,5 +39,12 @@ public class PacienteWS {
         Gson gson = new Gson();
         PacienteUpdateRequest request = gson.fromJson(json, PacienteUpdateRequest.class);
         return PacienteImp.actualizarPaciente(request);
+    }
+    
+    @Path("baja/{idPaciente}")
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    public Respuesta darDeBajaPaciente(@PathParam("idPaciente") int idPaciente) {
+        return PacienteImp.darDeBajaPaciente(idPaciente);
     }
 }
